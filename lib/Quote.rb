7e59e21455c15150b766b9aa100a7c5a3fd620fd
nbyrd9 <nianibyrd@gmail.com>
2021-02-03 16:53:51 -0500

    created new class

---
+++ lib/Quote.rb
@@ -0,0 +1,20 @@
+class Quote
+    attr_accessor :author, :quote
+   
+    @@all = []
+
+    def initialize(hash)
+         hash.each do |key, value|
+            self.send("#{key}=" , value)
+        end
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_quote(quote)
+        self.all.find{|info| info.quote == quote}
+    end       
+end
