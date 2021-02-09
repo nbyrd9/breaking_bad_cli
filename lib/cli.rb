91ab7af0d41ddf11adcef88ea52ca16f1640bfcf
nbyrd9 <nianibyrd@gmail.com>
2021-02-09 14:35:09 -0500

    updated functionality of user_input method

--- lib/cli.rb
+++ lib/cli.rb
@@ -52,25 +52,25 @@ class Cli
     
     def user_input(char)
         input = gets.chomp
-        if input == "1" ||  input.include?("Birthday")
+        if input == "1" ||  input.include?("Birthday") ||  input.include?("birthday")
             puts "#{char.name} was born on #{char.birthday}."
             yes_or_no
-        elsif input == "2" ||  input.include?("Occupation") 
+        elsif input == "2" ||  input.include?("Occupation") ||  input.include?("occupation")
             puts "Throughout the show, #{char.name} was a #{char.occupation}."
             yes_or_no
-        elsif input == "3" ||  input.include?("Status") 
+        elsif input == "3" ||  input.include?("Status") ||  input.include?("status")
             puts "#{char.name} was #{char.status} at the end of the series."
             yes_or_no
-        elsif input == "4" ||  input.include?("Appearance")
+        elsif input == "4" ||  input.include?("Appearance") ||  input.include?("appearance")
             puts "#{char.name} appeared in seasons #{char.appearance}."
             yes_or_no
-        elsif input == "5" ||  input.include?("Portrayed By")  || input.include?("Portrayed")
+        elsif input == "5" ||  input.include?("Portrayed By")  || input.include?("Portrayed") ||  input.include?("portrayed by")
             puts "#{char.name} is portrayed by #{char.portrayed}."
             yes_or_no
-        elsif input == "6" ||  input.include?("Image")  
+        elsif input == "6" ||  input.include?("Image")  ||  input.include?("image")
             puts "Click #{char.img} to see an image of #{char.name}."
             yes_or_no
-        elsif input == "7" ||  input.include?("Nickname")  
+        elsif input == "7" ||  input.include?("Nickname") ||  input.include?("nickname") 
             puts "#{char.name} is also referred to as #{char.nickname} throughout the show."
             yes_or_no
         else
